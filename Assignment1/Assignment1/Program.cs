c34daf7958e4be2327c6eb587035a59f5102d471
sonal <sonalkarle01@gmail.com>
2021-02-04 21:13:44 +0530

    Check emloyee present or absent

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace Assignment1
+namespace EmpWage
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee wage computation");
+            int IS_PRESENT = 1;   // Constant
+            Random random = new Random(); // Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
         }
     }
 }
