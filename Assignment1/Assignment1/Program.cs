b893904e0716d3d461ca08926052b0c64b0da33d
sonal <sonalkarle01@gmail.com>
2021-02-04 21:26:33 +0530

    Find employee wage for part time

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -7,17 +7,24 @@ namespace EmpWage
         static void Main(string[] args)
         {
             int IS_PRESENT = 1;   // Constant
+            int EMP_HR_PER_RATE = 20;       //Constant
+            int empHr = 0;            //Variable
+            int empwage = 0;          //Variable
             Random random = new Random(); // Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_PRESENT)
             {
                 Console.WriteLine("Employee is present");
+                empHr = 8;
 
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                empHr = 0;
             }
+            empwage = (EMP_HR_PER_RATE * empHr);
+            Console.WriteLine("Daily employee wage: " + empwage);
 
         }
     }
